0203c576af69c2efc0f6d99602df8980cbae1dff
Daniel Karaj <hey@krj.io>
2021-07-08 17:28:31 +0100

    use links

--- script.js
+++ script.js
@@ -5,20 +5,3 @@ document.getElementById("buttonenter").addEventListener("click", () => {
 document.getElementById("buttonnext").addEventListener("click", () => {
   document.body.classList.add("donenext");
 }, false);
-
-document.getElementById("bubble1").addEventListener("click", () => {
-  document.body.classList.add("done1");
-}, false);
-
-console.clear();
-
-var canvas = document.createElement('canvas'),
-    ctx = canvas.getContext('2d');
-
-var width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight;
-
-window.addEventListener('resize', function(){
-  width = canvas.width = window.innerWidth;
-  height = canvas.height = window.innerHeight;
-});
