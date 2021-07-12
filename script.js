283492c405d6195f154638fdf30a578cb8eb36fe
Daniel Karaj <hey@krj.io>
2021-07-12 21:20:53 +0100

    test transition

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ document.getElementById("buttonenter").addEventListener("click", () => {
 document.getElementById("buttonnext").addEventListener("click", () => {
   document.body.classList.add("donenext");
 }, false);
+
+document.querySelectorAll('li a').forEach((e) => {
+	e.addEventListener('click', (e) => {
+		document.body.classList.add('transitioning');
+	}, false)
+});
