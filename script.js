da352648a043f1c77ee34ea645898161be2f4595
Daniel Karaj <hey@krj.io>
2021-07-13 21:53:06 +0100

    revised intro

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-document.getElementById("buttonenter").addEventListener("click", () => {
+let buttonenter = document.getElementById("buttonenter");
+buttonenter.addEventListener("click", () => {
   document.body.classList.add("doneenter");
+  setTimeout(() => buttonenter.remove(), 500);
 }, false);
 
-document.getElementById("buttonnext").addEventListener("click", () => {
-  document.body.classList.add("donenext");
+document.getElementById("buttontoggleframework").addEventListener("click", () => {
+  document.body.classList.toggle("showframework");
+}, false);
+
+document.getElementById("buttonfilter").addEventListener("click", () => {
+  document.body.classList.toggle("filter");
 }, false);
 
 document.querySelectorAll('li a').forEach((e) => {
